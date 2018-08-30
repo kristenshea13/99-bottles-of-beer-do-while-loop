f5dabfbe6d764fdf3011b4d30d32795a608eba9d
Kristen Shea <kstar61@live.com>
2018-08-29 21:36:17 -0500

    99 bottles of beer as a do while loop

--- 99 bottles of beer do while/Program.cs
+++ 99 bottles of beer do while/Program.cs
@@ -6,7 +6,25 @@ namespace _99_bottles_of_beer_do_while
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sing along!");
+
+            int beerLeft = 100;
+            //declare variable to keep track of countdown of number of bottles of beer, since total will always be total - 1,
+            //start at 100 since song starts at 99
+
+            do
+            {
+                beerLeft = beerLeft - 1;
+                Console.WriteLine($"{beerLeft} bottles of beer on the wall. {beerLeft} bottles of beer. " +
+                    $"You take one down, pass it around, {beerLeft - 1} bottles of beer on the wall.");
+
+                
+
+            } while (beerLeft >= 2);
+
+
+
+            Console.ReadLine();
         }
     }
 }

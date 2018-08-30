0379bafc7c2654de14e48764ba4e4b6f7768e89a
Kristen Shea <kstar61@live.com>
2018-08-29 21:13:48 -0500

    Add project files.

---
+++ 99 bottles of beer do while/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _99_bottles_of_beer_do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
